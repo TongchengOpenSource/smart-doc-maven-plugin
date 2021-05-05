7b2e82c74007a6b6490395c804f8329fe7fab288
oppofind <836575280@qq.com>
2021-05-05 21:58:34 +0800

    support rpc push

---
+++ src/main/java/com/smartdoc/mojo/TornaRpcMojo.java
@@ -0,0 +1,47 @@
+/*
+ * smart-doc https://github.com/shalousun/smart-doc
+ *
+ * Copyright (C) 2018-2021 smart-doc
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.smartdoc.mojo;
+
+import com.power.doc.builder.rpc.RpcTornaBuilder;
+import com.power.doc.model.ApiConfig;
+import com.thoughtworks.qdox.JavaProjectBuilder;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+/**
+ * Send API documents to Torna
+ *
+ * @author yu 2021/5/05.
+ */
+@Mojo(name = "torna-rpc", requiresDependencyResolution = ResolutionScope.COMPILE)
+public class TornaRpcMojo extends BaseDocsGeneratorMojo {
+
+    @Override
+    public void executeMojo(ApiConfig apiConfig, JavaProjectBuilder javaProjectBuilder) {
+        try {
+            RpcTornaBuilder.buildApiDoc(apiConfig, javaProjectBuilder);
+        } catch (Exception e) {
+            getLog().error(e);
+        }
+    }
+}
